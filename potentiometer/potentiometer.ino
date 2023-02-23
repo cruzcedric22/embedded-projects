c1bde0247daa98f0ea286d4244d5287b591801e3
ced <cruzcedric66@gmail.com>
2023-02-23 17:46:23 +0800

    potentiomter w/ nottes and pic

--- potentiometer/potentiometer.ino
+++ potentiometer/potentiometer.ino
@@ -1,3 +1,13 @@
+// NOTES FOR PROGRAMMER BY DANERIS AND FRIENDS 
+// COLOR WIRE IDENTIFIERS:
+// BLACK = GND
+// RED = 5V
+// GREEN = DIGITAL PIN
+// USE PWM PIN WHEN USING POTENTIOMETER (GREEN WIRE)
+
+
+
+
 int pot;
 int led;
 void setup() {
@@ -13,5 +23,7 @@ void loop() {
   led = map(pot, 0, 1023, 0, 255);
 
   analogWrite(5, led);
+
+ 
 
 }
